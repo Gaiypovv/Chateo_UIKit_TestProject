e64f87997a031041fcdd4a11d1fb9cef360dd04d
Gaiypovv <eldargaiypov@gmail.com>
2024-02-20 22:00:32 +0600

    Initial Commit

---
+++ Chateo_UIKit_TestProject/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Chateo_UIKit_TestProject
+//
+//  Created by Eldar Gaiypov on 20/2/24.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
